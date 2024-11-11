797ddb39cc7c61d63a09df4d3ceb9dfdc525a3f9
Slava.Gvsh <slava.gvsh@gmail.com>
2024-11-11 17:32:30 +0100

    fix Router3

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ScrollToTop from "./utils/scrollToTop";
 function App() {
   return (
     <div className="app">
-      <Router>
+      <Router basename="/Portfolio">
         <ScrollToTop />
         <Navbar />
         <Routes>
