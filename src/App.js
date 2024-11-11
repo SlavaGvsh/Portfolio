7e8a8ec8e499409a9b70a92484cb08d7e22d0a0c
Slava.Gvsh <slava.gvsh@gmail.com>
2024-11-11 18:21:31 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ScrollToTop from "./utils/scrollToTop";
 function App() {
   return (
     <div className="app">
-      <Router basename="/portfolio">
+      <Router basename="/Portfolio">
         <ScrollToTop />
         <Navbar />
         <Routes>
